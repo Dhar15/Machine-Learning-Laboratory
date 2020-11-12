f641ea10cc56dbaec5161d0929e720c54d07960e
Kshitij Dhar <39898582+Dhar15@users.noreply.github.com>
2020-11-12 19:54:39 +0530

    Update naivebayes_docs.py

--- 6/naivebayes_docs.py
+++ 6/naivebayes_docs.py
@@ -1,3 +1,7 @@
+''' Q.6) Assuming a set of documents that need to be classified, use the naïve Bayesian Document Classifier model to perform this task. 
+         Built-in Java classes/API can be used to write the program.
+         Calculate the accuracy, precision, and recall for your data set. '''
+
 import pandas as pd
 
 msg = pd.read_csv('naivetext1.csv', names=['message','label'])
