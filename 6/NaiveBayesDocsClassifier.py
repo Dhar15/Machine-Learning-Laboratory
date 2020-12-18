654541a48ec98d4e8c749bde8f9c0441b7a1197c
Kshitij Dhar <39898582+Dhar15@users.noreply.github.com>
2020-12-19 00:00:26 +0530

    Update and rename naivebayes_docs.py to NaiveBayesDocsClassifier.py

--- 6/NaiveBayesDocsClassifier.py
+++ 6/NaiveBayesDocsClassifier.py
@@ -1,6 +1,5 @@
 ''' Q.6) Assuming a set of documents that need to be classified, use the naïve Bayesian Document Classifier model to perform this task. 
-         Built-in Java classes/API can be used to write the program.
-         Calculate the accuracy, precision, and recall for your data set. '''
+         Built-in Java classes/API can be used to write the program. Calculate the accuracy, precision, and recall for your data set. '''
 
 import pandas as pd
 
